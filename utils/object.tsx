8760e0c8806366dbb0cd77f933d93f498e231b87
yangyao <873241789@qq.com>
2018-08-22 09:49:54 +0800

    新增 utils方法

---
+++ utils/object.tsx
@@ -0,0 +1,15 @@
+/**
+ * Copyright: Copyright (C) 2018 sitb.software,All Rights Reserved
+ * author: yangyao(873241789@qq.com)
+ * date: 2018/8/22
+ */
+export default class {
+    /**
+     * 深拷贝 转化成字符串的方法
+     * @param obj 对象或者数组
+     * @returns {any}
+     */
+    static clone(obj: any) {
+        return JSON.parse(JSON.stringify(obj));
+    }
+}
